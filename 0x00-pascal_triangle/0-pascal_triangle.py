64423f3ab4bd3a0423aceaeea50266c48e2711e2
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-07-26 05:53:01 +0300

    pascal

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+0-Pascal triangle
+"""
+
+def pascal_triangle(n):
+    """
+    Returns the first n rows of Pascal's triangle.
+    """
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
