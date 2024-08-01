ad7bb84d6bffd556d4015cb7c63eacf1a6bf3447
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-08-01 19:44:50 +0300

    lockbox

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    # Initialize variables
+    n = len(boxes)
+    unlocked = [False] * n
+    unlocked[0] = True  # Start by unlocking the first box
+    keys = boxes[0]  # Start with the keys from the first box
+    queue = keys[:]  # Initialize the queue with keys from the first box
+
+    while queue:
+        key = queue.pop(0)  # Get the next key to process
+        if key < n and not unlocked[key]:  # Check if the box can be unlocked
+            unlocked[key] = True  # Mark the box as unlocked
+            queue.extend(boxes[key])  # Add new keys to the queue
+
+    return all(unlocked)  # Return True if all boxes are unlocked, otherwise False
